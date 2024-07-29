60b5505a326028e8296835cdf93b598051d1dd71
abdulmoeedsiddiqi <u2023009@giki.edu.pk>
2024-07-29 16:18:24 +0500

    Program for Multiplication Table

---
+++ Multiplicatio_Table.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n << "x" << i << "=" << n * i << endl;
+    }
+    return 0;
+}
