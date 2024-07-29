64ee560e19e882ffda46dfa99a93c3e072d2e1a5
abdulmoeedsiddiqi <u2023009@giki.edu.pk>
2024-07-29 18:03:50 +0500

    Data types

---
+++ Datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int i=10 ;
+    float f=5.5;
+    char c='$';
+    string s="Pakistan";
+    short int si=12;
+    long int li = 12220 ;
+    unsigned int ui= 34;    // for 0 or positive
+    unsigned long int uli= 343 ;  // for 0 or positive
+    long  lf = 3.333 ; 
+    double d =2323.3;
+    cout<<" "<<i<<" "<<f<<" "<<c<<" "<<si<<" "<<li<<" "<<ui<<" "<<uli<<" "<<lf<<" "<<d<<endl;
+    return 0;
+}
---
+++ sizeof_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    cout<<"The size of int is "<<sizeof(int)<<endl;
+    cout<<"The size of short is "<<sizeof(short)<<endl;
+    cout<<"The size of long is "<<sizeof(long)<<endl;
+    cout<<"The size of char is "<<sizeof(char)<<endl;
+    cout<<"The size of float is "<<sizeof(float)<<endl;
+    cout<<"The size of double is "<<sizeof(double)<<endl;
+    cout<<"The size of long double is "<<sizeof(long double)<<endl;
+    return 0;
+}
