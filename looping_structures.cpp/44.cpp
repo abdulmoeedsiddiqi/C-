403404ec8d1b4abf839d6d3d905e82dabeb73e32
abdulmoeedsiddiqi <u2023009@giki.edu.pk>
2024-08-19 00:16:36 +0500

    -

---
+++ looping_structures.cpp/44.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,sum;
+    for(int i=1 ; i<=5 ; i++){
+        cout<<'1';
+        sum=1;
+        for(j=2;j<=i;j++){
+            cout<<'+'<<j;
+            sum+=j;
+        }
+        cout<<'='<<sum<<endl;
+    }
+    return 0;
+}
